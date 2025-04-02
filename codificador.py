f1a07a3ad68d93a9acfdcaeae04dfd98d1972757
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2025-04-01 21:43:30 -0600

    Avances para el codificador en las funciones

--- codificador.py
+++ codificador.py
@@ -1,8 +1,36 @@
+def bienvenida():
+    print("Bienvenido a nuestro codificador")
+    print("Creado por: Santiago Gonzalez y Patron Garcia")
+    print("Escribe un comando o 'quit' para salir ")
 
-def test():
-    return "testing"
+
+def comandos(comando, llave):
+    partes = comando.strip().split()
+
+    if partes == "":
+        return "ERROR! Expresión no válida", llave
+
+    instruccion = partes[0]
+
+    if instruccion == "quit":
+        return "quit", llave
+    else: 
+        return "comando no reconocido", llave
 
 def main():
-    print(test())
+    llave = ""
+    bienvenida()
+
+    while True:
+        entrada = input("codificador >> ").strip()
+        resultado, llave = comandos(entrada, llave)
+
+        if resultado == "quit":
+            print("Saliendo ...")
+            print("Gracias por usar nuestro codificador.")
+            break
+        else:
+            print("resultado >>", resultado)
+
 
 main()
